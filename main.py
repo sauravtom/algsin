9a56d627a810648ec9a21bb14cae3a95c03056a8
saurav <saurav@issac.(none)>
2013-10-08 17:00:03 +0530

    cleaned a lot of code, clubbed the previously separate Image-page class into the main handler one, and url scheme now is /?page= and /?i=

--- main.py
+++ main.py
@@ -32,8 +32,12 @@ def reddit_scrape(url):
             f = "http://i."+f.split('/')[2]+"/"+f.split('/')[3]+".jpg"
             arr.append( [ i["data"]["title"] , f ])
 
+    # appends fake url in the array
     for i in arr:
-        f = shortapi.shorten( i[1]+";"+i[0] )
+        i[0]=i[0].encode('ascii', 'ignore')
+        i[1]=i[1].encode('ascii', 'ignore')
+        f = shortapi.shorten( i[1]+';'+i[0] )
+        f = f.split('/')[-1]
         i.append(f)
 
     return arr
@@ -43,11 +47,13 @@ class Matrix(db.Expando):
     data = db.TextProperty() #using text since string can only store 500 characters
 
 
+# declaring array in global scope
 arr=[]
 try:
     q = db.GqlQuery("SELECT * FROM Matrix")
     for i in q:
         arr = json.loads(i.data)
+        logging.info('database updated for the first time')
 except:
     self.redirect('/db')
 
@@ -57,38 +63,49 @@ class MainHandler(webapp2.RequestHandler):
         try :   n = int(self.request.get('page'))
         except ValueError:  n=1
 
-        template = jinja_environment.get_template('templates/index.html')
-        self.response.out.write(template.render(arr=arr,n=n))
+        try :   i = self.request.get('i')
+        except ValueError:  i=""
 
-class Image_page(webapp2.RequestHandler):
-    def get(self):
-        fake_url = self.request.url
-        bcode = fake_url.split('/')[-1]
-        #bitly_url = self.request.get('u')
-        bitly_url = "http://bit.ly/" + bcode 
-        #except ValueError:  self.redirect('/')
+        if i == "":
+            template = jinja_environment.get_template('templates/index.html')
+            self.response.out.write(template.render(arr=arr,n=n))
+        else:
+            #fake_url = self.request.url
+            #bcode = fake_url.split('/')[-1]
+            bitly_url = "http://bit.ly/" + i
 
-        fp = urllib2.urlopen(bitly_url)
-        url=fp.geturl()
+            fp = urllib2.urlopen(bitly_url)
+            url=fp.geturl()
 
-        title = url.split(';')[-1]
-        url = url.split(';')[0]
+            title = url.split(';')[-1]
+            title = urllib2.unquote(title)
+            url = url.split(';')[0]
 
-        template = jinja_environment.get_template('templates/image.html')
-        self.response.out.write(template.render(url=url,title=title))
+            template = jinja_environment.get_template('templates/image.html')
+            self.response.out.write(template.render(url=url,title=title))
+
 
 class Update_DB(webapp2.RequestHandler):
     def get(self):
         arr = reddit_scrape('http://www.reddit.com/user/TakSlak/m/sfwporn/.json?limit=200')
         
+        try: 
+            #flush the database
+            q = db.GqlQuery("SELECT * FROM Matrix")
+            for i in q:
+                i.delete()
+            logging.info('Database successfully flushed')
+        except:
+            logging.error('error flushing the database')    
+
         try:
             foo=Matrix()
             foo.data = json.dumps(arr)
             foo.put()
             logging.info('Database successfully updated')
         except HTTPError: 
+            logging.error('error writing to database')
             self.redirect('/')
-
 
         self.response.out.write("Database updated and size : " + str(len(arr)))
         
@@ -96,7 +113,7 @@ class Update_DB(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/db',Update_DB),
-    ('/i',Image_page)
+    ('/i/',Image_page)
 ], debug=True)
 
 
@@ -104,11 +121,15 @@ app = webapp2.WSGIApplication([
 only store those links in db which ends with .jpg or .png [DONE]
 (transform image links to imgur links) [DONE]
 
-update database every 12 hours
+update database every 12 hours [DONE]
 
-make separate page for image permalinks
+make separate page for image permalinks [DONE]
 
-ad try catch in /db
+ad try catch in /db [DONE]
+
+require login for /db
+
+Add 404 page
 
 '''
 
