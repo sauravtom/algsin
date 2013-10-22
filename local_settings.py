58936584303d6083cc07ac4aa9fa244085f65935
Saurav Tomar <Stomatrix@gmail.com>
2013-10-22 11:23:32 +0530

    Update local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -1,6 +1,6 @@
 BitlyKey = {
-        'login': 'o_3b2iboubhc',
-        'apikey': 'R_4103f1b24a29455679b24b49add9ba41'
+        'login': '',
+        'apikey': ''
         }
 
 bitly_access_token = '8e0b7a79e572c4725fc5045ee8d9a85113a24ea8'
